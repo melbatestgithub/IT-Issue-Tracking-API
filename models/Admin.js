82c45ca4c61fcff925b20d7d1c6880f2ba2150db
melakuzeleke <melakuzeleke443@gmail.com>
2024-05-29 21:47:01 +0300

    model

--- models/Admin.js
+++ models/Admin.js
@@ -4,7 +4,12 @@ const AdminSchema=new mongoose.Schema({
     firstName:{type:String,required:true},
     lastName:{type:String,required:true},
     password:{type:String},
-    isAdmin:{type:Boolean}
+    isAdmin:{type:Boolean},
+    phone:{type:String},
+    address:{type:String},
+    username:{type:String},
+    profileImg:{type:String}
+
 })
 const Admin=mongoose.model("Admin,",AdminSchema)
 module.exports=Admin
