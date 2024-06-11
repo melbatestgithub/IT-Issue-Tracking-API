20d8b7185b2f0c487722495611a5d4514f064214
melakuzeleke <melakuzeleke443@gmail.com>
2024-06-11 19:05:36 +0300

    commit

--- models/Issues.js
+++ models/Issues.js
@@ -1,17 +1,17 @@
-const mongoose=require('mongoose')
+const mongoose = require('mongoose');
 
-const IssueSchema=new mongoose.Schema({
-    title:{type:String,required:true},
-    description:{type:String,required:true},
-    category:{type:String},
-    status:{type:String,default:"pending"},
-    userId:{type:String},
-    date:{type:Date},
-    department:{type:String},
-    roomNumber:{type:Number},
-    assignedTo:{type:String},
-    priority:{type:String}
-    
-})
-const Issue= mongoose.model("Issue",IssueSchema)
-module.exports=Issue
+const IssueSchema = new mongoose.Schema({
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    category: { type: String },
+    status: { type: String, default: "pending" },
+    userId: { type: String },
+    date: { type: Date, default: Date.now }, // Assuming this is the creation date
+    department: { type: String },
+    roomNumber: { type: Number },
+    assignedTo: { type: String },
+    priority: { type: String }
+}, { timestamps: true }); // This will automatically add createdAt and updatedAt fields
+
+const Issue = mongoose.model("Issue", IssueSchema);
+module.exports = Issue;
